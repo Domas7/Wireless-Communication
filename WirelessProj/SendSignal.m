72784543ebe7f6f0dae266131c3c684fa5886873
baptistegrr <122107948+baptistegrr@users.noreply.github.com>
2023-11-16 14:35:29 +0100

    Add files via upload

--- WirelessProj/SendSignal.m
+++ WirelessProj/SendSignal.m
@@ -1,10 +1,27 @@
 function power_cost = SendSignal(type,distance)
-%SENDSIGNAL Summary of this function goes here
+%SENDSIGNAL 
+% calculating the power lost through the transmission depending on the type
+% of environment
 %   Detailed explanation goes here
+    if type ==1 %%open env
+        shadowing_std_dev = 1; % Standard deviation of log-normal shadowing in dB
+        multipath_std_dev = 2; % Standard deviation of multipath fading in dB
+        path_loss_exponent=2;
+    elseif type ==2 %%forest
+        shadowing_std_dev = 4; % Standard deviation of log-normal shadowing in dB
+        multipath_std_dev = 2; % Standard deviation of multipath fading in dB
+        path_loss_exponent=2.5;
+    elseif type ==3  %%mountain
+        shadowing_std_dev = 3; % Standard deviation of log-normal shadowing in dB
+        multipath_std_dev = 7; % Standard deviation of multipath fading in dB
+        path_loss_exponent=3;
+    elseif type ==4  %%lake
+        shadowing_std_dev = 1; % Standard deviation of log-normal shadowing in dB
+        multipath_std_dev = 4; % Standard deviation of multipath fading in dB
+        path_loss_exponent=1;
+    end
     frequency = 2.4e9; % Signal frequency in Hertz (2.4 GHz for WiFi)
-    shadowing_std_dev = 7; % Standard deviation of log-normal shadowing in dB
-    multipath_std_dev = 7; % Standard deviation of multipath fading in dB
-    path_loss_exponent=type;
+    
     % Path loss calculation
         
     % Path loss
--- WirelessProj/calcLossEnv.m
+++ WirelessProj/calcLossEnv.m
@@ -1,10 +1,10 @@
-function power_after = calcLossEnv(in,dist,n,power)
+function power_after = calcLossEnv(in,dist,type,power)
 %CALCLOSSENV 
 % Calculate the power lost through an environment
 %   Detailed explanation goes here
     power_after=power;
     if in==true
-        loss=SendSignal(n,dist);
+        loss=SendSignal(type,dist);
         power_after=power-loss;
     end
 end
